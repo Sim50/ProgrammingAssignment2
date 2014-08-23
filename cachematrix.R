856d6d7559fe3ae259506959d2512289e25b4e39
Simon <simon.coursera@web.de>
2014-08-23 08:37:13 +0200

    First approach for ProgAssigment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
+## creates a chached matrix 'object' w/ setter and getter methods
+## example
+## > source("cachematrix.R")
+## > mat <- matrix(1:4,2,2)
+## > matrixObject<-makeCacheMatrix(mat)
+## > cacheSolve(matrixObject) ## 1st time inverse is calculated
+##       [,1] [,2]
+## [1,]   -2  1.5
+## [2,]    1 -0.5
+## > cacheSolve(matrixObject) ## 2nd time inverse is retrieved from cache
+## getting cached data
+## [,1] [,2]
+## [1,]   -2  1.5
+## [2,]    1 -0.5
 
-## Write a short comment describing this function
 
+## makeCacheMatrix takes a (square) matrix as input and calculates the inverse. A list 
+## of set and get methods is returned
 makeCacheMatrix <- function(x = matrix()) {
-
+      minv <- NULL
+      set <- function(y) {
+            x <<- y
+            minv <<- NULL
+      }
+      get <- function() x
+      setminv <- function(inverse) minv <<- solve(x)
+      getminv <- function() minv
+      list(set = set, get = get,
+           setminv = setminv,
+           getminv = getminv)
 }
 
 
-## Write a short comment describing this function
+
+## cacheMatrix takes a matrixobject created by makeCacheMatrix and returns the inverse of the
+## matrix either from cache if it was calculated before or by calculation
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+      minv <- x$getminv()
+      if(!is.null(minv)) {
+            message("getting cached data")
+            return(minv)
+      }
+      data <- x$get()
+      minv <- solve(data, ...)
+      x$setminv(minv)
+      minv
 }
